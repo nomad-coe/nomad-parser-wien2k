6e0eca4f95824c397d07a432f5b430ae9d48692d
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-07-27 11:03:05 +0200

    parsing number of atoms

--- parser/parser-wien2k/wien2k_parser.py
+++ parser/parser-wien2k/wien2k_parser.py
@@ -37,14 +37,24 @@ mainFileDescription = SM(
            repeats = True,
            required = True,
            forwardMatch = True,
-           sections   = ['section_run'],
+           sections   = ['section_run', 'section_single_configuration_calculation'],
            subMatchers = [
-               SM(name = 'header',
-                  startReStr = r"\s*:LABEL[0-9]+: using WIEN2k_(?P<x_wien2k_version>[0-9.]+) \(Release (?P<x_wien2k_release_date>[0-9/.]+)\) in ",
-                  sections=["x_wien2k_header"],
-                  fixedStartValues={'program_name': 'WIEN2k', 'program_basis_set_type': '(L)APW+lo' }
+               SM(
+                   name = 'header',
+                   startReStr = r"\s*:LABEL[0-9]+: using WIEN2k_(?P<x_wien2k_version>[0-9.]+) \(Release (?P<x_wien2k_release_date>[0-9/.]+)\) in ",
+                   sections=["x_wien2k_header"],
+                   fixedStartValues={'program_name': 'WIEN2k', 'program_basis_set_type': '(L)APW+lo' }
+              ), SM(
+                  name = "scf iteration",
+                  startReStr = r"\s*:ITE(?P<x_wien2k_iteration_number>[0-9]+):\s*[0-9]*. ITERATION",
+                  sections=["section_scf_iteration"],
+                  subMatchers=[
+                      SM(r":NATO :\s*(?P<x_wien2k_number_of_independent_atoms>[0-9]+)INDEPENDENT AND\s*(?P<x_wien2k_total_atoms>[0-9]+)\s*TOTAL ATOMS IN UNITCELL"),
+                      SM("\s*SUBSTANCE: (?P<x_wien2k_system_name>.*)")
+                  ]
               )
-           ])
+           ]
+       )
     ])
 
 # loading metadata from nomad-meta-info/meta_info/nomad_meta_info/fhi_aims.nomadmetainfo.json

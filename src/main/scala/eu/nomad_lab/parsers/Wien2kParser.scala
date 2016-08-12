41b6882ed42a1fc91c9518917bb7f279bb2126ad
Tomecka, Daria M. (datom) <tomeckadm@gmail.com>
2016-08-12 18:08:21 +0200

    Replace Wien2kParser.scala

--- src/main/scala/eu/nomad_lab/parsers/Wien2kParser.scala
+++ src/main/scala/eu/nomad_lab/parsers/Wien2kParser.scala
@@ -30,8 +30,6 @@ object Wien2kParser extends SimpleExternalParserGenerator(
     "parser-wien2k/wien2k_parser_in0.py",
     "parser-wien2k/wien2k_parser_in1c.py",
     "parser-wien2k/wien2k_parser_in2c.py",
-    "parser-wien2k/wien2k_parser_in1.py",
-    "parser-wien2k/wien2k_parser_in2.py",
     "parser-wien2k/wien2k_parser_struct.py",
     "parser-wien2k/setup_paths.py",
     "nomad_meta_info/public.nomadmetainfo.json",

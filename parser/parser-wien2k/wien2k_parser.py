8644010a7b196df89de89d8305fb8e39a2d883bf
Daria Tomecka <zarvok79@yahoo.com>
2017-05-16 12:12:37 +0200

    adding license header

--- parser/parser-wien2k/wien2k_parser.py
+++ parser/parser-wien2k/wien2k_parser.py
@@ -11,6 +11,21 @@ import wien2k_parser_struct, wien2k_parser_in0, wien2k_parser_in1c,  wien2k_pars
 # This is the parser for the main output file (.scf) of WIEN2k.
 ################################################################
 
+# Copyright 2016 The NOMAD Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 
 class Wien2kContext(object):
     """context for wien2k parser"""
